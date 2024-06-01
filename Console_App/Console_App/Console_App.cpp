eec3fdc8f0d88cbe3f811bcb44d8b737260f2979
Pavel Gornostaev <42691529+Pavreally@users.noreply.github.com>
2024-06-02 00:20:23 +0400

    Correction of inaccuracies

--- Console_App/Console_App/Console_App.cpp
+++ Console_App/Console_App/Console_App.cpp
@@ -1,12 +1,5 @@
 // Console_App.cpp
 
-/***
-* Не реализовано:
-* - Таймер проверки файлов
-* - Удаление файлов
-* - Глобальная критическая секция
-*/
-
 #include <fstream>
 #include <string>
 #include <Windows.h>
@@ -15,16 +8,16 @@
 // Debug
 #include <iostream>
 
-// Подключаем класс с 64-битной переменной "id" типа long long
+// 64-bit Number
 #include "ThreadEntity.h"
 
-// Определяем количество потоков
+// Set the Maximum Number of Threads
 #define MAX_THREADS 2
 
 using namespace std;
 
-DWORD WINAPI GetFileList(LPVOID lpParam);
-int GetRandomInt(int x, int y);
+DWORD WINAPI GetFileList(LPVOID LpParam);
+int GetRandomInt(int Start, int End);
 
 int main()
 {
@@ -35,7 +28,7 @@ int main()
 
     HANDLE threadArray[MAX_THREADS];
     
-    // Создание потоков и передача им объектов
+    // Create Threads
     for (int i = 0; i < MAX_THREADS; i++)
     {
         threadArray[i] = CreateThread(
@@ -47,35 +40,35 @@ int main()
             nullptr
         );
 
-        // Проверяем актуальность потоков
+        // Checking the Thread Status
         if (threadArray[i] == nullptr)
         {
             ExitProcess(3);
         }
     }
 
-    // Ожидаем завершение потоков
+    // Wait for the Array of Threads to Complete
     WaitForMultipleObjects(2, threadArray, true, INFINITE);
 }
 
-// Функция потока
-DWORD WINAPI GetFileList(LPVOID lpParam)
+// Streaming Function
+DWORD WINAPI GetFileList(LPVOID LpParam)
 {
-    // Адаптируем lpParameter
-    ThreadEntity* instance = static_cast<ThreadEntity*>(lpParam);
+    // Convert Parameter
+    ThreadEntity* Instance = static_cast<ThreadEntity*>(LpParam);
 
-    // Случайная пауза
+    // Get a Random Number
     Sleep(GetRandomInt(2, 4));
 
-    // Поиск файлов в директории
-    string filePath = "./files";
-    string fileExtension = "txt";
-    string searchField = filePath + "\\*";
+    // Create a Log File
+    string FilePath = "./files";
+    string FileExtension = "txt";
+    string SearchField = FilePath + "\\*";
 
-    ofstream logfile(filePath + "/list.log");
+    ofstream LogFile(FilePath + "/list.log");
 
     WIN32_FIND_DATAA fileData;
-    HANDLE hFind = FindFirstFileA(searchField.c_str(), &fileData);
+    HANDLE hFind = FindFirstFileA(SearchField.c_str(), &fileData);
 
     if (hFind == INVALID_HANDLE_VALUE)
     {
@@ -84,17 +77,17 @@ DWORD WINAPI GetFileList(LPVOID lpParam)
     }
     else
     {
-        // Проверяем файлы
+        // Send Data to a File
         do
         {
             const string FILE_NAME = fileData.cFileName;
             if (!(fileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
             {
-                // Исключаем все файлы, кроме целевого расширения
-                if (FILE_NAME.substr(FILE_NAME.find_last_of(".") + 1) == fileExtension)
+                // Search for Only the Specified Extension
+                if (FILE_NAME.substr(FILE_NAME.find_last_of(".") + 1) == FileExtension)
                 {
-                    logfile << FILE_NAME << endl;
-                    logfile << instance->id << endl;
+                    LogFile << FILE_NAME << endl;
+                    LogFile << Instance->id << endl;
                 }
             }
         } while (FindNextFileA(hFind, &fileData) != 0);
@@ -102,17 +95,16 @@ DWORD WINAPI GetFileList(LPVOID lpParam)
         FindClose(hFind);
     }
 
-    logfile.close();
+    LogFile.close();
 
     return 0;
 }
 
-// Генерируем случайное число
-int GetRandomInt(int x, int y)
+int GetRandomInt(int Start, int End)
 {
     random_device rd;
     mt19937 gen(rd());
-    uniform_int_distribution<> dis(x, y);
+    uniform_int_distribution<> dis(Start, End);
 
     return dis(gen);
 }
